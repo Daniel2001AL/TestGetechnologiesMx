793c123d7a3acd458b5e0befd10f412d5161d639
DanielRios <193290@ids.upchiapas.edu.mx>
2022-06-18 15:20:24 -0500

    add form register and validations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PruebaComponent } from './prueba/prueba.component';
+import { ModalComponent } from './modal/modal.component';
+import { FormregistroComponent } from './formregistro/formregistro.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PruebaComponent,
+    ModalComponent,
+    FormregistroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
